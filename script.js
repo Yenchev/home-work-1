b935e3d3c096063661edf407363b0e9dc517bf8b
Illia <illiayenchev@gmail.com>
2022-06-05 14:35:06 +0200

    work complite

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 'use srtict';
 
-let numberOfFilms = prompt('Скільки фільмів ви вже переглянули?');
+let numberOfFilms = +prompt('Скільки фільмів ви вже переглянули?', '');
 
 let personalMovieDB = {
 	count: numberOfFilms,
 	movies: {},
 	actors: {},
 	genres: [],
-	private: false,
+	privat: false,
 };
 
 let i;
@@ -15,9 +15,7 @@ let filmPrompt = () => {
 	for (i = 0; i < 2; i++) {
 		let lastFilm1 = prompt('Один з останніх фільмів?');
 		let rateFilm = prompt('Оцініть його?');
-		personalMovieDB.movies[i] = `${lastFilm1}: ${rateFilm}`;
+		personalMovieDB.movies[lastFilm1] = rateFilm;
 	}
 };
 filmPrompt();
-
-// console.log(personalMovieDB);
